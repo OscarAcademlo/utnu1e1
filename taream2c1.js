ad8db3505c0f9913420bad8d271079674b79af65
OscarAcademlo <107896200+OscarAcademlo@users.noreply.github.com>
2022-12-25 00:16:03 -0300

    Add files via upload

---
+++ taream2c1.js
@@ -0,0 +1,19 @@
+    const fondo = document.getElementById("uno")
+    fondo.addEventListener("click",colorBack);
+    const fondo1 = document.getElementById("dos")
+    fondo1.addEventListener("click",colorBack1);  
+    const fondo2 = document.getElementById("tres")
+    fondo2.addEventListener("click",colorBack2);  
+ 
+function colorBack() {
+    let body = document.getElementsByTagName('body')[0]
+   body.style.backgroundColor='brown'
+}
+function colorBack1() {
+    let body = document.getElementsByTagName('body')[0]
+   body.style.backgroundColor='blueviolet'
+}
+function colorBack2() {
+    let body = document.getElementsByTagName('body')[0]
+   body.style.backgroundColor='rgb(42, 165, 44)'
+}
